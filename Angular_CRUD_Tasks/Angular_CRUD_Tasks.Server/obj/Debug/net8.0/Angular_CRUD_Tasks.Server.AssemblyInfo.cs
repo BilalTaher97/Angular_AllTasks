6ad84b82512e9e6aa374da53765b9df708fc4b92
BilalTaher97 <belalkhasawneh97@gmail.com>
2025-03-24 01:21:57 +0300

    Add New Task

--- Angular_CRUD_Tasks/Angular_CRUD_Tasks.Server/obj/Debug/net8.0/Angular_CRUD_Tasks.Server.AssemblyInfo.cs
+++ Angular_CRUD_Tasks/Angular_CRUD_Tasks.Server/obj/Debug/net8.0/Angular_CRUD_Tasks.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Angular_CRUD_Tasks.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4d5bcd950f5466da62a78c7e22547e4b0bf9a0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Angular_CRUD_Tasks.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Angular_CRUD_Tasks.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
